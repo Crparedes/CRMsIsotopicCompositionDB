2297bb98ec70806f0c95b76ba30e9735d2aad48a
Cristhian Alfredo Paredes Cardona <caparedes@inm.local>
2023-02-22 18:36:10 -0500

    more info and certificates. We should split administrative from measurement results data. Start to think into D-CRM-certificates

--- www/IsotopicData/02_INITIAL_DataTableConstruction.R
+++ www/IsotopicData/02_INITIAL_DataTableConstruction.R
@@ -1,12 +1,8 @@
 INITI_IsoComCRMTable <- read.csv(file ='www/IsotopicData/RAW_INITIAL_IsoComCRM.csv')
 INITI_IsoComCRMTable['Element'][INITI_IsoComCRMTable['Element'] == ''] <- NA
 INITI_IsoComCRMTable['CRM.name'][INITI_IsoComCRMTable['CRM.name'] == ''] <- NA
-INITI_IsoComCRMTable['CRM.lot'][INITI_IsoComCRMTable['CRM.lot'] == ''] <- NA
-INITI_IsoComCRMTable['CRM.producer'][INITI_IsoComCRMTable['CRM.producer'] == ''] <- NA
-INITI_IsoComCRMTable['CRM.description'][INITI_IsoComCRMTable['CRM.description'] == ''] <- NA
-INITI_IsoComCRMTable['CRM.presentation'][INITI_IsoComCRMTable['CRM.presentation'] == ''] <- NA
-INITI_IsoComCRMTable['UncertType'][INITI_IsoComCRMTable['UncertType'] == ''] <- NA
-INITI_IsoComCRMTable['k.factor'][INITI_IsoComCRMTable['k.factor'] == ''] <- NA
+
 
 INITI_IsoComCRMTable <- zoo::na.locf(INITI_IsoComCRMTable)
 
+# PArtir la tabla en info y resultados, aparteee
