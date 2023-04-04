ec9fb51d29245e6e84b448f045d1098eaffe828b
Cristhian Alfredo Paredes Cardona <caparedes@inm.local>
2023-04-04 11:51:47 -0500

    Changed the MySQL free database that was extremely slow when deployed, for googlesheets4 package and a google sheet than contains all information

---
+++ Modules/00_GoogleSheets_Functions.R
@@ -0,0 +1,37 @@
+## See https://stackoverflow.com/a/70215575/7612904
+if (FALSE) { # Run this only once
+  options(gargle_oauth_cache = ".secrets")
+  gs4_auth()
+  list.files(".secrets/")
+  gs4_deauth()
+}
+gs4_auth(cache = ".secrets", email = "craparedesca@unal.edu.co")
+
+
+## See secttion 6 of https://shiny.rstudio.com/articles/persistent-data-storage.html#mysql
+SHEET_ID <- 'https://docs.google.com/spreadsheets/d/1vpIlGMA3rLVD3ScpQyXVphLEeCsxW_xjcZeo75crM34/edit#gid=979538683'
+
+saveData <- function(tableName, data) {
+  # The data must be a dataframe rather than a named vector
+  data <- data %>% as.list() %>% data.frame()
+  # Add the data as a new row
+  sheet_append(ss = SHEET_ID, data = data, sheet = tableName)
+}
+
+loadFromDataBase <- function(tableName, element = NULL, CRM = NULL) {
+  table <- data.frame(read_sheet(ss = SHEET_ID, sheet = tableName))
+  if (missing(element) && missing(CRM)) {
+    return(table)
+  } else {
+    if (missing(CRM)) {
+      return(table[grep(tolower(element), table$Elements, value = FALSE), ])
+    } else {
+      return(table[table$CRM_name == CRM, ])
+    }
+  }
+}
+
+# loadFromDataBase(tableName = 'IsoCompCRM_Info')
+# loadFromDataBase(tableName = 'IsoCompCRM_Info', element = 'uranium')
+# loadFromDataBase(tableName = 'IsoCompCRM_Info', CRM = 'UCLO-1')
+
--- Modules/00_MySQL_Functions.R
+++ Modules/00_MySQL_Functions.R
@@ -1,55 +1,55 @@
-## See secttion 5 of https://shiny.rstudio.com/articles/persistent-data-storage.html#mysql
-
-options(mysql = list("host" = "sql9.freesqldatabase.com", "user" = "sql9599488", "password" = "PHsEvvEBuY"))
-
-saveData <- function(data) {
-  # Connect to the database
-  MRCsICDB <- dbConnect(RMySQL::MySQL(), user = "sql9599488", password = "PHsEvvEBuY",
-                        dbname = "sql9599488", host = "sql9.freesqldatabase.com")
-  
-  # Construct the update query by looping over the data fields
-  query <- sprintf(
-    "INSERT INTO %s (%s) VALUES ('%s')",
-    table, 
-    paste(names(data), collapse = ", "),
-    paste(data, collapse = "', '")
-  )
-  
-  # Submit the update query and disconnect
-  dbGetQuery(MRCsICDB, query)
-  dbDisconnect(MRCsICDB)
-}
-
-loadFromDataBase <- function(tableName, element = NULL, CRM = NULL) {
-  # Connect to the database
-  MRCsICDB <- dbConnect(RMySQL::MySQL(), user = "sql9599488", password = "PHsEvvEBuY",
-                        dbname = "sql9599488", host = "sql9.freesqldatabase.com")
-  
-  # Construct the fetching query
-  if (missing(element) && missing(CRM)) {
-    query <- sprintf("SELECT * FROM %s", tableName)
-  } else {
-    if (missing(CRM)) {
-      query <- sprintf("SELECT * FROM %s WHERE Elements LIKE '%%%s%%'", tableName, element)
-    } else {
-      query <- sprintf("SELECT * FROM %s WHERE CRM_name LIKE '%%%s%%'", tableName, CRM)
-    }
-    
-  }
-  # Submit the fetch query and disconnect
-  data <- dbGetQuery(MRCsICDB, query)
-  dbDisconnect(MRCsICDB)
-  return(data)
-}
-
-# loadFromDataBase(tableName = 'MatrixCRM_DataIR')
-# loadFromDataBase(tableName = 'MatrixCRM_DataIR', element = 'lead')
-# library(microbenchmark)
-# library(ggthemes)
-# library(ggplot2) 
+# ## See secttion 5 of https://shiny.rstudio.com/articles/persistent-data-storage.html#mysql
 # 
-# tm <- microbenchmark(loadFromDataBase('IsoCompCRM_Info', element = 'lead'), 
-#                      TrimTableElement(loadFromDataBase('IsoCompCRM_Info'), element = 'lead'), 
-#                      times = 100)  
-# print(tm)
-# autoplot(object = tm)
+# options(mysql = list("host" = "sql9.freesqldatabase.com", "user" = "sql9599488", "password" = "PHsEvvEBuY"))
+# 
+# saveData <- function(data) {
+#   # Connect to the database
+#   MRCsICDB <- dbConnect(RMySQL::MySQL(), user = "sql9599488", password = "PHsEvvEBuY",
+#                         dbname = "sql9599488", host = "sql9.freesqldatabase.com")
+#   
+#   # Construct the update query by looping over the data fields
+#   query <- sprintf(
+#     "INSERT INTO %s (%s) VALUES ('%s')",
+#     table, 
+#     paste(names(data), collapse = ", "),
+#     paste(data, collapse = "', '")
+#   )
+#   
+#   # Submit the update query and disconnect
+#   dbGetQuery(MRCsICDB, query)
+#   dbDisconnect(MRCsICDB)
+# }
+# 
+# loadFromDataBase <- function(tableName, element = NULL, CRM = NULL) {
+#   # Connect to the database
+#   MRCsICDB <- dbConnect(RMySQL::MySQL(), user = "sql9599488", password = "PHsEvvEBuY",
+#                         dbname = "sql9599488", host = "sql9.freesqldatabase.com")
+#   
+#   # Construct the fetching query
+#   if (missing(element) && missing(CRM)) {
+#     query <- sprintf("SELECT * FROM %s", tableName)
+#   } else {
+#     if (missing(CRM)) {
+#       query <- sprintf("SELECT * FROM %s WHERE Elements LIKE '%%%s%%'", tableName, element)
+#     } else {
+#       query <- sprintf("SELECT * FROM %s WHERE CRM_name LIKE '%%%s%%'", tableName, CRM)
+#     }
+#     
+#   }
+#   # Submit the fetch query and disconnect
+#   data <- dbGetQuery(MRCsICDB, query)
+#   dbDisconnect(MRCsICDB)
+#   return(data)
+# }
+# 
+# # loadFromDataBase(tableName = 'MatrixCRM_DataIR')
+# # loadFromDataBase(tableName = 'MatrixCRM_DataIR', element = 'lead')
+# # library(microbenchmark)
+# # library(ggthemes)
+# # library(ggplot2) 
+# # 
+# # tm <- microbenchmark(loadFromDataBase('IsoCompCRM_Info', element = 'lead'), 
+# #                      TrimTableElement(loadFromDataBase('IsoCompCRM_Info'), element = 'lead'), 
+# #                      times = 100)  
+# # print(tm)
+# # autoplot(object = tm)
